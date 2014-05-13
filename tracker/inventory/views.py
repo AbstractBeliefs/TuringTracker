5b0676e1ea5da12ba874a754bb3750431da8ce82
Gareth Pulham <garethpulham@gmail.com>
2014-05-13 19:09:55 +0100

    Add the summary page url rules and views.

--- tracker/inventory/views.py
+++ tracker/inventory/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from inventory.models import *
+from django.contrib.auth.models import User
+
+def index(request):
+    stockedDevices = Device.objects.exclude(deviceState=Device.Shipped).count()
+    readyDevices = Device.objects.filter(deviceState=Device.Shippable).count()
+
+    context = {
+        "recordedDevices": Device.objects.count(),
+        "volunteers": User.objects.count(),
+        "donors": Donor.objects.count(),
+        "stocked": stockedDevices,
+        "shippable": min(readyDevices, 100),
+        "unshippable": min(stockedDevices - readyDevices, 100-min(readyDevices, 100)),
+    }
+
+    return render(request, "index.html", context)
--- tracker/tracker/urls.py
+++ tracker/tracker/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'tracker.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'', include("inventory.urls")),
     url(r'^admin/', include(admin.site.urls)),
 )
